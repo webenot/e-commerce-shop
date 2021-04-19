d2604b97a5557a9767290e313e37a00c343e269a
Halyna Urbanska <webenot@mail.ua>
2021-04-19 12:57:01 +0300

    feat: Add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { Login } from 'Pages/auth/Login';
+import { Register } from 'Pages/auth/Register';
+import { Home } from 'Pages/Home';
 
 export const App = () => (
-  <div>
-    <p>React App</p>
-  </div>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/register" component={Register} />
+  </Switch>
 );
---
+++ client/src/pages/auth/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Login = () => (
+  <div>
+    <p>Login</p>
+  </div>
+);
