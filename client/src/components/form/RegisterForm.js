d483b0090441ed487041a58ba995bf8e96d83185
Halyna Urbanska <webenot@mail.ua>
2021-04-22 17:54:50 +0300

    feat: Handle register form submit fix: Fix firebase config chore: Add dependencies

--- client/src/components/form/RegisterForm.js
+++ client/src/components/form/RegisterForm.js
@@ -1,26 +1,45 @@
 import React, { useState, useCallback } from 'react';
 import { MDBInput, MDBBtn } from 'mdbreact';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { auth } from 'App/firebase';
 
 export const RegisterForm = () => {
   const [ email, setEmail ] = useState('');
 
-  const handleSubmit = useCallback(() => {
-    //
-  }, []);
+  const handleSubmit = useCallback(async e => {
+    e.preventDefault();
+    const config = {
+      url: 'http://localhost:3000/register/complete',
+      handleCodeInApp: true,
+    };
+    try {
+      await auth.sendSignInLinkToEmail(email, config);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // save user email in local storage
+    toast.success(`Email is send to ${email}. Click the link to complete your registration`);
+    window.localStorage.setItem('emailForSignIn', email);
+    // clear state
+    setEmail('');
+    return false;
+  }, [ email ]);
 
   const handleInputChange = useCallback(e => {
     setEmail(e.target.value);
   }, []);
 
   return (
-    <form onSubmit={handleSubmit()}>
+    <form onSubmit={handleSubmit}>
       <MDBInput
         label="Email"
         type="email"
         value={email}
         onChange={handleInputChange}
         autoFocus
-        color="teal"
       />
       <MDBBtn className="button-raised" type="submit">Register / {email}</MDBBtn>
     </form>
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { ToastContainer } from 'react-toastify';
 
 import { RegisterForm } from 'Components/form/RegisterForm';
 
@@ -8,6 +9,7 @@ export const Register = () => (
     <MDBRow>
       <MDBCol md="6" className="offset-md-3">
         <h4>Register</h4>
+        <ToastContainer />
         <RegisterForm />
       </MDBCol>
     </MDBRow>
