ce6c6ff3a46cd41846a2e344cb74628737d8d6a8
Halyna Urbanska <webenot@mail.ua>
2021-04-22 15:01:27 +0300

    feat: Add header menu

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import { Login } from 'Pages/auth/Login';
 import { Register } from 'Pages/auth/Register';
 import { Home } from 'Pages/Home';
+import { Header } from 'Components/nav/Header';
 
 export const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/register" component={Register} />
-  </Switch>
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  </>
 );
---
+++ client/src/components/nav/Header.js
@@ -0,0 +1,37 @@
+import React, { useState, useCallback } from 'react';
+import { Menu } from 'antd';
+import { HomeOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
+const {
+  SubMenu,
+  Item,
+} = Menu;
+
+export const Header = () => {
+  const [ current, setCurrent ] = useState('home');
+
+  const handleClick = useCallback(e => {
+    setCurrent(e.key);
+  });
+
+  return (
+    <header>
+      <Menu onClick={handleClick} selectedKeys={[ current ]} mode="horizontal">
+        <Item key="home" icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Item>
+        <Item key="register" icon={<UserAddOutlined />} className="float-end">
+          <Link to="/register">Register</Link>
+        </Item>
+        <Item key="login" icon={<UserOutlined />} className="float-end">
+          <Link to="/login">Login</Link>
+        </Item>
+        <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+          <Item key="setting:1">Option 1</Item>
+          <Item key="setting:2">Option 2</Item>
+        </SubMenu>
+      </Menu>
+    </header>
+  );
+};
