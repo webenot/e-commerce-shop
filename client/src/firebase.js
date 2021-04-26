553569f85ea8c1b11a4ad894e2eaaed4df397efe
Halyna Urbanska <webenot@mail.ua>
2021-04-26 12:24:54 +0300

    fix: Fix firebase authentication configuring fix: Change firebase project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import firebase from 'firebase';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCIOUO5PA5B6WeAOFoWKBUnI3IzsyIhWUw',
-  authDomain: 'ecommerce-shop-f4842.firebaseapp.com',
-  projectId: 'ecommerce-shop-f4842',
-  storageBucket: 'ecommerce-shop-f4842.appspot.com',
-  messagingSenderId: '787481753264',
-  appId: '1:787481753264:web:82bb8153d5c84338b52d75',
-  measurementId: 'G-38FKN2G8XW',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
