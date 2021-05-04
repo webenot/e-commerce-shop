7067da5e96b2ec6f6dd04f02cd95384834294494
Halyna Urbanska <webenot@mail.ua>
2021-05-04 12:36:10 +0300

    feat: Add category CRUD server operations

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -17,17 +17,54 @@ module.exports.create = async (req, res) => {
 };
 
 module.exports.read = async (req, res) => {
-
+  try {
+    const { slug } = req.params;
+    const category = await Category.findOne({ slug });
+    res.json(category);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Get category failed');
+  }
 };
 
 module.exports.update = async (req, res) => {
-
+  try {
+    const { slug } = req.params;
+    const { name } = req.body;
+    const updated = await Category.findOneAndUpdate(
+      { slug },
+      {
+        $set: {
+          name,
+          slug: slugify(name),
+        },
+      },
+      { new: true },
+    );
+    res.json(updated);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Update category failed');
+  }
 };
 
 module.exports.remove = async (req, res) => {
-
+  try {
+    const { slug } = req.params;
+    const categoryDeleted = await Category.findOneAndDelete({ slug });
+    res.json(categoryDeleted);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Remove category failed');
+  }
 };
 
 module.exports.list = async (req, res) => {
-
+  try {
+    const categories = await Category.find().sort({ createdAt: -1 });
+    res.json(categories);
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Get categories list failed');
+  }
 };
